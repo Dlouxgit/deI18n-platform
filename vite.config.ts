e457192cd82a1b1437dc5957d883328910497bc8
Dlouxgit <470490778@qq.com>
2024-11-01 13:01:20 +0800

    feat: add https

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+  server: {
+    proxy: {}
+},
   plugins: [
     remix({
       future: {
@@ -12,5 +16,6 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    basicSsl()
   ],
 });
